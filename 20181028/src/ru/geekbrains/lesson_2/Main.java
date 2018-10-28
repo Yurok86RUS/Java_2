3454db7fee0b4486df6580a24b552b7aa9078d88
Yurok86RUS <yurok.kolesnikov@gmail.com>
2018-10-28 20:33:05 +0500

    create Exception MyArraySizeException

--- 20181028/src/ru/geekbrains/lesson_2/Main.java
+++ 20181028/src/ru/geekbrains/lesson_2/Main.java
@@ -13,23 +13,35 @@ public class Main {
         System.out.print("Введите размерность двумерного массива: ");
         size = scanner.nextInt();
         System.out.println();
-        initArray();
-
+        try {
+            initArray();
+        } catch (MyArraySizeException e) {
+            e.printStackTrace();
+        }
     }
 
-    public static void initArray(){
-        array = new int[size][size];
-        int tmp = 0;
-        for (int i = 0; i < size; i++){
-            for (int j = 0; j < size; j++) {
-                array[i][j] = tmp;
-                tmp++;
-                if (array[i][j] < 10) {
-                    System.out.print(array[i][j] + "    ");
+    public static void initArray() throws MyArraySizeException {
+        if (size != 4) throw new MyArraySizeException("Размерность не 4", size);
+            array = new int[size][size];
+            int tmp = 0;
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    array[i][j] = tmp;
+                    tmp++;
+                    if (array[i][j] < 10) {
+                        System.out.print(array[i][j] + "    ");
+                    } else System.out.print(array[i][j] + "   ");
                 }
-                else System.out.print(array[i][j] + "   ");
+                System.out.println();
             }
-            System.out.println();
+    }
+
+    static class MyArraySizeException extends Exception{
+        private int badSize;
+
+        public MyArraySizeException(String message, int badSize) {
+            super(message);
+            this.badSize = badSize;
         }
     }
 
