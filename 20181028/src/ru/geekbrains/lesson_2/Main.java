11ad48b2fcd44e8b4a3070530efb3c0906fe286d
Yurok86RUS <yurok.kolesnikov@gmail.com>
2018-10-28 21:15:54 +0500

    1) create class sumOfArray; 2) create exception MyArrayDataException

--- 20181028/src/ru/geekbrains/lesson_2/Main.java
+++ 20181028/src/ru/geekbrains/lesson_2/Main.java
@@ -2,9 +2,11 @@ package ru.geekbrains.lesson_2;
 
 import java.util.Scanner;
 
+import static java.lang.Integer.parseInt;
+
 public class Main {
 
-    private static int[][] array;
+    private static String[][] array;
     public static Scanner scanner = new Scanner(System.in);
     public static int size;
 
@@ -19,22 +21,44 @@ public class Main {
             System.out.println(e.getMessage());
             System.out.println("Введенная размерность: " + e.getBadSize());
         }
+
+        try {
+            sumOfArray(array);
+        }catch (MyArrayDataException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Ошибка в элементе: " + "[" + e.getRow() + "]" + "[" + e.getCol() + "]");
+        }
+
     }
 
     public static void initArray() throws MyArraySizeException {
         if (size != 4) throw new MyArraySizeException("Размерность не 4", size);
-            array = new int[size][size];
+            array = new String[size][size];
             int tmp = 0;
             for (int i = 0; i < size; i++) {
                 for (int j = 0; j < size; j++) {
-                    array[i][j] = tmp;
+                    array[i][j] = Integer.toString(tmp);
                     tmp++;
-                    if (array[i][j] < 10) {
+//                    if (array[i][j] < Integer.toString(10)) {
                         System.out.print(array[i][j] + "    ");
-                    } else System.out.print(array[i][j] + "   ");
+//                    } else System.out.print(array[i][j] + "   ");
                 }
                 System.out.println();
             }
+        System.out.println();
+    }
+
+    public static void sumOfArray(String[][] array) throws MyArrayDataException {
+
+        int sum = 0;
+
+        for (int i = 0; i < array.length; i++){
+            for (int j = 0; j < array.length; j++){
+                sum += parseInt(array[i][j]);
+            }
+        }
+
+        System.out.println("Сумма элементов массива = " + sum);
     }
 
     static class MyArraySizeException extends Exception{
@@ -44,6 +68,25 @@ public class Main {
         public MyArraySizeException(String message, int badSize) {
             super(message);
             this.badSize = badSize;
+        }
+    }
+
+    static class MyArrayDataException extends Exception{
+        private int row;
+        private int col;
+
+        public int getRow() {
+            return row;
+        }
+
+        public int getCol() {
+            return col;
+        }
+
+        public MyArrayDataException(String message, int row, int col) {
+            super(message);
+            this.row = row;
+            this.col = col;
         }
     }
 
