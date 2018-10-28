c9f2a710a3026a497c4fce991abea5b12fb3c298
Yurok86RUS <yurok.kolesnikov@gmail.com>
2018-10-28 19:09:16 +0500

    не смог реализовать doIt

---
+++ 20181024/src/Marathon/Course.java
@@ -0,0 +1,13 @@
+package Marathon;
+
+public class Course{
+
+    Barrier[] barriers = {new Cross(50), new Wall(2), new Water(2)};
+
+    public Course() {
+    }
+
+    public void doIt(Team team) {
+
+        }
+}
---
+++ 20181024/src/Marathon/Mouse.java
@@ -0,0 +1,7 @@
+package Marathon;
+
+public class Mouse extends Animal {
+    public Mouse(String name) {
+        super("Мышь", name, 100, 1, 1);
+    }
+}
