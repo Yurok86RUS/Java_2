717ddb8614749625bc9e7954a40fc8bf235ae4f8
Yurok86RUS <yurok.kolesnikov@gmail.com>
2018-10-28 20:02:06 +0500

    create class initArray

--- 20181028/src/ru/geekbrains/lesson_2/Main.java
+++ 20181028/src/ru/geekbrains/lesson_2/Main.java
@@ -1,8 +1,38 @@
 package ru.geekbrains.lesson_2;
 
+import java.util.Scanner;
+
 public class Main {
+
+    private static int[][] array;
+    public static Scanner scanner = new Scanner(System.in);
+    public static int size;
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("Введите размерность двумерного массива: ");
+        System.out.println();
+        size = scanner.nextInt();
+        initArray();
+
     }
+
+    public static void initArray(){
+        array = new int[size][size];
+        int tmp = 0;
+        for (int i = 0; i < size; i++){
+            for (int j = 0; j < size; j++) {
+                array[i][j] = tmp;
+                tmp++;
+                if (array[i][j] < 10) {
+                    System.out.print(array[i][j] + "    ");
+                }
+                else System.out.print(array[i][j] + "   ");
+            }
+            System.out.println();
+        }
+    }
+
+    
+
 }
