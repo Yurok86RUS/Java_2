2965b4237b62ece0c6892f69b982be78dd356f62
Yurok86RUS <yurok.kolesnikov@gmail.com>
2018-11-01 22:42:12 +0500

    create method deleteReplays

--- HW_3 20181101/src/ru/geekbrains/lesson_3/Main.java
+++ HW_3 20181101/src/ru/geekbrains/lesson_3/Main.java
@@ -1,12 +1,22 @@
 package ru.geekbrains.lesson_3;
 
+import java.util.ArrayList;
+import java.util.Set;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-      String[] array = {"null", "one", "two", "three", "four", "one", "five",
+      String[] array = {"null", "one", "two", "tree", "four", "one", "five",
                         "tree", "six", "seven", "one", "six", "eight", "nine"};
+      System.out.println("Задание 1: ");
 
-      new Task_1().transformArray(array);
+      ArrayList <String> newArray = new Task_1().transformArray(array);
+        System.out.println("Исходный массив: " + newArray);
+
+      Set a = new Task_1().deleteReplays(newArray);
+        System.out.println("Убрали все повторы:" + a);
+
+        
     }
 }
--- HW_3 20181101/src/ru/geekbrains/lesson_3/Task_1.java
+++ HW_3 20181101/src/ru/geekbrains/lesson_3/Task_1.java
@@ -1,17 +1,26 @@
 package ru.geekbrains.lesson_3;
 
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class Task_1 {
 
     public Task_1() {
     }
 
-    public void transformArray(String[] arr){
+    public ArrayList <String> transformArray(String[] arr){
         ArrayList<String> arrayList = new ArrayList<>();
         for (String s : arr){
             arrayList.add(s);
         }
-        System.out.println(arrayList);
+        return arrayList;
     }
+
+    public Set<String> deleteReplays (ArrayList<String> arrayList){
+        Set<String> s = new LinkedHashSet<>(arrayList);
+        return s;
+    }
+
+
 }
