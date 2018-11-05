94960127e21eb59a090cfde5daf33b1eed0bdef4
Yurok86RUS <yurok.kolesnikov@gmail.com>
2018-11-05 23:38:46 +0500

    закончил. не смог реализовать изменение стиля написания. возможно это нельзя сделать в данных классах

--- HW_4 20181104/src/ru/geekbrains/lesson_4/Chat.java
+++ HW_4 20181104/src/ru/geekbrains/lesson_4/Chat.java
@@ -1,13 +1,12 @@
 package ru.geekbrains.lesson_4;
 
-import javafx.scene.layout.GridPane;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+
 
 public class Chat extends JFrame{
 
@@ -48,56 +47,6 @@ public class Chat extends JFrame{
         sendPanel.add(sendMessage, BorderLayout.NORTH);
         sendPanel.add(stylePanel,BorderLayout.SOUTH);
 
-
-        JButton buttonSmile = new JButton(":)");
-        buttonSmile.setBackground(Color.yellow);
-        buttonSmile.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-        JButton buttonCry = new JButton(";(");
-        buttonCry.setBackground(Color.yellow);
-        buttonCry.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-        JButton buttonSadness = new JButton(":(");
-        buttonSadness.setBackground(Color.yellow);
-        buttonSadness.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-        smilePanel.add(buttonSmile);
-        smilePanel.add(Box.createVerticalStrut(10));
-        smilePanel.add(buttonCry);
-        smilePanel.add(Box.createVerticalStrut(10));
-        smilePanel.add(buttonSadness);
-
-        //наполнение stylePanel
-        stylePanel.setLayout(new GridLayout());
-        JButton bold = new JButton("Жирный");
-        bold.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-        JButton italics = new JButton("Курсив");
-        italics.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-        stylePanel.add(bold);
-        stylePanel.add(italics);
-
         //основное окно
         JTextArea mainWindow = new JTextArea();
         mainWindow.setEditable(false);
@@ -131,6 +80,61 @@ public class Chat extends JFrame{
         sendMessage.add(insertText);
         sendMessage.add(send);
 
+        JButton buttonSmile = new JButton(":)");
+        buttonSmile.setBackground(Color.yellow);
+        buttonSmile.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            insertText.setText(insertText.getText() + " :)");
+            insertText.grabFocus();
+            }
+        });
+        JButton buttonCry = new JButton(";(");
+        buttonCry.setBackground(Color.yellow);
+        buttonCry.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                insertText.setText(insertText.getText() + " ;(");
+                insertText.grabFocus();
+            }
+        });
+        JButton buttonSadness = new JButton(":(");
+        buttonSadness.setBackground(Color.yellow);
+        buttonSadness.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                insertText.setText(insertText.getText() + " :(");
+                insertText.grabFocus();
+            }
+        });
+        smilePanel.add(buttonSmile);
+        smilePanel.add(Box.createVerticalStrut(10));
+        smilePanel.add(buttonCry);
+        smilePanel.add(Box.createVerticalStrut(10));
+        smilePanel.add(buttonSadness);
+
+        //наполнение stylePanel
+        stylePanel.setLayout(new GridLayout());
+        JButton bold1 = new JButton("Жирный");
+        bold1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        JButton italics = new JButton("Курсив");
+        italics.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+//                JFormattedTextField textField = new JFormattedTextField();
+//                String s = insertText.getText();
+//                textField.setFont(new Font("Aral",Font.BOLD,15));
+//                textField.setText(s);
+
+            }
+        });
+        stylePanel.add(bold1);
+        stylePanel.add(italics);
 
         //верхняя строка
         JLabel info = new JLabel("Домашнее задание по четвертому уроку");
