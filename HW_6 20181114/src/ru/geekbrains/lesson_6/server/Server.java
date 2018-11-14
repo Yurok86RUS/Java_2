8c9b2533988ba7c9754e8336b435dc5fc2e36de3
Yurok86RUS <yurok.kolesnikov@gmail.com>
2018-11-14 14:27:34 +0500

    create echo server

---
+++ HW_6 20181114/src/ru/geekbrains/lesson_6/server/Server.java
@@ -0,0 +1,38 @@
+package ru.geekbrains.lesson_6.server;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Server {
+    public static void main(String[] args) {
+        ServerSocket serverSocket = null;
+        Socket socket = null;
+
+        try {
+            serverSocket = new ServerSocket(7852);
+            System.out.println("Server 7852 started...");
+            socket = serverSocket.accept();
+            System.out.println("Client connected");
+            Scanner scanner = new Scanner(socket.getInputStream());
+            PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
+
+            while (true){
+                String string = scanner.nextLine();
+                if (string.equals("/end")) break;
+                printWriter.println(string);
+                printWriter.flush();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                serverSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
