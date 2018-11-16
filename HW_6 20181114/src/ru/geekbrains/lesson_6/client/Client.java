184f7f97a61a4a8aed181184e08c7a97a562f375
Yurok86RUS <yurok.kolesnikov@gmail.com>
2018-11-16 12:08:48 +0500

    finish task 6

--- HW_6 20181114/src/ru/geekbrains/lesson_6/client/Client.java
+++ HW_6 20181114/src/ru/geekbrains/lesson_6/client/Client.java
@@ -1,58 +1,74 @@
 package ru.geekbrains.lesson_6.client;
 
-import javafx.fxml.Initializable;
-
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.net.URL;
-import java.util.ResourceBundle;
-import java.util.Scanner;
 
-public class Client implements Initializable {
+public class Client {
+    public static void main(String[] args) throws IOException {
 
-        Socket socket;
-        //DataInputStream dataInputStream;
-        //DataOutputStream dataOutputStream;
+        ClientConnected clientConnected = new ClientConnected();
 
-        private Scanner in;
-        private PrintWriter out;
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    clientConnected.readMessage();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    clientConnected.sengMsg();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    static class ClientConnected{
+        Socket socket = null;
+        BufferedReader in = null;
+        PrintWriter out = null;
+        BufferedReader console = null;
+        String serverMsg, clientMsg;
 
         final String IP_ADDRESS = "localhost";
         final int PORT = 7777;
 
-        @Override
-        public void initialize (URL location, ResourceBundle resources){
+        public ClientConnected() throws IOException {
+            socket = new Socket(IP_ADDRESS, PORT);
+            System.out.println("You connected to server 7777.");
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream(), true);
+            console = new BufferedReader(new InputStreamReader(System.in));
+        }
 
-            try {
-                socket = new Socket(IP_ADDRESS, PORT);
-                //dataInputStream = new DataInputStream(socket.getInputStream());
-                //dataOutputStream = new DataOutputStream(socket.getOutputStream());
-                in = new Scanner(socket.getInputStream());
-                out = new PrintWriter(socket.getOutputStream());
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        while (true) {
-                            try {
-                                //String string = dataInputStream.readUTF();
-                                //if (string.equals("/serverclosed")) break;
-                                //System.out.println(string);
-                                String string = in.nextLine();
-                                if (in.equals("/serverclosed")) break;
-                                System.out.println(string);
-                            } finally {
-                                try {
-                                    socket.close();
-                                } catch (IOException e) {
-                                    e.printStackTrace();
-                                }
-                            }
-                        }
-                    }
-                }).start();
-            } catch (IOException e) {
-                e.printStackTrace();
+        void sengMsg() throws IOException{
+            System.out.print("Write you message: ");
+            while (true){
+                if ((clientMsg = console.readLine()) != null){
+                    out.println(clientMsg);
+                    if (clientMsg.equalsIgnoreCase("/exit")) break;
+                }
             }
         }
+
+        void readMessage() throws IOException{
+            while (true){
+                if ((serverMsg = in.readLine()) != null){
+                    System.out.println(serverMsg);
+                }
+            }
         }
+
+    }
+    }
--- HW_6 20181114/src/ru/geekbrains/lesson_6/server/Server.java
+++ HW_6 20181114/src/ru/geekbrains/lesson_6/server/Server.java
@@ -1,40 +1,105 @@
 package ru.geekbrains.lesson_6.server;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
 
 public class Server {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Serv serv = new Serv();
+        serv.serverStart();
+        serv.waitClient();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true){
+                    String txt = null;
+                    try {
+                        txt = serv.in.readLine();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    if (txt != null){
+                        try {
+                            serv.sendMsg(txt);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    serv.writeConsole();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    static class Serv{
+        BufferedReader in = null;
+        PrintWriter out = null;
         ServerSocket serverSocket = null;
         Socket socket = null;
+        BufferedReader console = null;
 
-        try {
-            serverSocket = new ServerSocket(7777);
-            System.out.println("Server 7777 started...");
-            socket = serverSocket.accept();
-            System.out.println("Client connected");
-            Scanner scanner = new Scanner(socket.getInputStream());
-            PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
+        void serverStart() {
+                try {
+                    serverSocket = new ServerSocket(7777);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    System.out.println("Error open port 7777");
+                }
+            System.out.println("Server 7777 sterted...");
+        }
 
-            while (true){
-                String string = scanner.nextLine();
-                if (string.equals("/end")) break;
-                //printWriter.println(string);
-                System.out.println(string);
-                printWriter.flush();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
+        void waitClient() throws IOException{
             try {
-                serverSocket.close();
-                socket.close();
+                socket = serverSocket.accept();
+                System.out.println("Client connected.");
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println("Client not found");
+            }
+
+            try {
+                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                out = new PrintWriter(socket.getOutputStream(),true);
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
+
+        void sendMsg(String msg) throws IOException  {
+            if (msg.equalsIgnoreCase("/exit")) close();
+            out.println("Server: " + msg);
+            System.out.println(msg);
+        }
+
+        void close() throws IOException{
+            in.close();
+            out.close();
+            socket.close();
+            serverSocket.close();
+        }
+
+        void writeConsole() throws IOException{
+            while (true){
+                console = new BufferedReader(new InputStreamReader(System.in));
+                String txt = console.readLine();
+                sendMsg(txt);
+            }
+        }
+
     }
 }
